344c974e1ea85b7af81e2bd811c848cf193af609
RomaZe <rzolotov@yandex.ru>
2018-12-04 17:16:00 +0300

    Add Cat() class

--- probe_1.py
+++ probe_1.py
@@ -7,12 +7,21 @@ class Snow:
     def __str__(self):
         return "Snow color: {color}, how much: {height}".format(color=self.color, height=self.hight)
 
+class Cat:
+
+    def __init__(self):
+        self.sound = "Myau"
+
+    def __str__(self):
+        return "Cat can {}".format(self.sound)
 
 def total(a, b):
     return a + b
 
 
 day1 = Snow('white', 10)
+murzik = Cat()
 print(day1)
+print(murzik)
 
 
