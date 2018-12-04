121712cd058aee59ffbe3d1135ad0839fba47ce5
RomaZe <rzolotov@yandex.ru>
2018-12-04 17:07:55 +0300

    Add class Snow()

--- probe_1.py
+++ probe_1.py
@@ -1,8 +1,20 @@
+class Snow:
+
+    def __init__(self, color, hight):
+        self.color = color
+        self.hight = hight
+
+    def __str__(self):
+        return "Snow color: {color}, how much: {height}".format(color=self.color, height=self.hight)
+
+
 def total(a, b):
     return a + b
 
 
+day1 = Snow('white', 10)
 x = total(5, 4)
 print(x)
+print(day1)
 
 
